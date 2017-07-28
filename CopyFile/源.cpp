d67870ae3bf7cf053d5a8b422c15633a8493177e
liuqihong <1510649869@qq.com>
2017-07-28 15:10:30 +0800

    Signed-off-by: liuqihong <1510649869@qq.com>

--- CopyFile/源.cpp
+++ CopyFile/源.cpp
@@ -1,13 +1,7 @@
-#include "Transfer.h"
-#include<algorithm>
+
+#include<stdio.h>
 #include<iostream>
-#include<string>
-#include <mutex>
-#include <thread>
-using namespace std;
-int main() {
-	std::string src = "D:\\新建文件夹";
-	std::string des = "E:\\ER";
-	CopyAllFolderAndFile(src,des,".*");
-	return 0;
+int main(int argc,char** argv)
+{
+    return 0;
 }
